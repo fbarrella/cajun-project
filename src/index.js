ff8ed4a9fb5fc53e129ae93c7f71b3b2a2e12ad4
Felipe B. Netto <felipe.netto@mobiauto.com.br>
2019-08-10 15:50:45 -0300

    implemented file loader for webpack and inserted images into carousel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import 'owl.carousel';
+import './style/base.css';
 import * as _ from 'lodash';
+import './assets/melhor-que-tocar-sozinho.jpg';
+import './assets/melhor-remedio.jpg';
+import './assets/musica-juntos.jpg';
 
 $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
@@ -11,3 +15,27 @@ $(document).ready(function () {
         dots: true
     });
 });
+
+document.querySelector('.open-menu').addEventListener("click", () => {
+    let secondMenu = document.querySelector('.menu-second');
+    let secondMenuStatus = secondMenu.style.display;
+    if(_.isEqual(secondMenuStatus, "flex")){
+        secondMenu.style.display = "none";
+    }else{
+        secondMenu.style.display = "flex";
+    }
+});
+
+window.onscroll = function() {
+    setSticky();
+};
+let navbar = document.querySelector('header');
+let sticky = navbar.offsetTop;
+
+function setSticky() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+}
